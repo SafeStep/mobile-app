89df545465f52a00ba5418793d547bd692cc18d2
OlliePugh <oliver.pugh@mac.com>
2021-03-22 22:41:51 +0000

    Added support for mapbox

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,101 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { Component, useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import mbxClient from "@mapbox/mapbox-sdk";
+import mbxDirections from "@mapbox/mapbox-sdk/services/directions";
+import polyline from "@mapbox/polyline";
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
-}
+const baseClient = mbxClient({ accessToken: "sk.eyJ1Ijoib3B1Z2giLCJhIjoiY2tta3dmbnc2MTUzcDJ3azVmOHhxMDk2cSJ9.XP_hR3TpDYj9i5HMz5WzwA" });
+const directionsClient = mbxDirections(baseClient);
+
+MapboxGL.setAccessToken("");
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  map: {
+    flex: 1,
+    width: "100%"
+  }
 });
+
+interface geoJSON {
+}
+
+const makeGeoJSON: (data: number[][]) => any = function(data) {
+
+  data.forEach(function(part, index, arr) {
+    arr[index] = part.reverse();
+  });
+
+  return {
+    "type":"Feature",
+    "properties":{},
+    "geometry":{
+      "type":"LineString",
+      "coordinates": data
+    }
+  }
+};
+
+const App: React.FC = () => {
+  const [path, setPath] = useState([] as number[][]);
+
+  useEffect(() => {
+    getRoute().then((_path: number[][]) => {
+      setPath(_path);
+      console.log(_path);
+    }).catch((error: any) => {
+      console.warn(error);
+    });
+  }, []);  // run like component did mount
+
+  return (
+      <View style={styles.page}>
+        <MapboxGL.MapView style={styles.map}>
+          <MapboxGL.ShapeSource id='line1' shape={makeGeoJSON(path)}>
+            <MapboxGL.LineLayer id="test" style={{"lineColor": "red"}} />
+          </MapboxGL.ShapeSource>
+        </MapboxGL.MapView>
+      </View>
+  );
+};
+
+
+export default App;
+
+const getRoute = (): Promise<number[][]> => {
+  return new Promise((resolve, reject) => {
+    directionsClient.getDirections({
+      profile: 'walking',
+      overview: "full",
+      waypoints: [
+        {
+          coordinates: [-1.213787, 52.771881],  // in order of lat long (East, North)
+        },
+        {
+          coordinates: [-1.2321, 52.7651],
+        },
+        // {
+        //   coordinates: [13.4194, 52.5072],
+        // }
+      ]
+    }).send()
+    .then((response: any) => {
+      const directions = response.body;
+      console.log(directions);
+      return resolve(polyline.decode(directions.routes[0].geometry));
+    })
+    .catch((error: string) => {
+      reject(error)
+    });
+  })
+}
+
+//sk.eyJ1Ijoib3B1Z2giLCJhIjoiY2tta3dmbnc2MTUzcDJ3azVmOHhxMDk2cSJ9.XP_hR3TpDYj9i5HMz5WzwA
