4b657628e86e1b3cfc7884b3dfc4781785c8eba2
Noted70 <bamlakkal2002@gmail.com>
2021-03-30 01:55:08 +0100

    Update signup.tsx

--- screens/signup.tsx
+++ screens/signup.tsx
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingTop: '30%'
+        paddingTop: '40%'
     },
     stretch: {
         width: '100%',
