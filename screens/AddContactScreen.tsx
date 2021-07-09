7e8d18589649e87c09eb4204e570b6e90b97f8b7
Ollie Pugh <oliver.pugh@mac.com>
2021-07-09 22:49:19 +0100

    Added inputs

--- screens/AddContactScreen.tsx
+++ screens/AddContactScreen.tsx
@@ -1,62 +1,27 @@
-import React, { useState, useRef, FC} from "react";
+import React, { FC, useState } from "react";
 import {
-  SafeAreaView,
-  StyleSheet,
   View,
-  StatusBar,
-  TouchableOpacity,
-  Text,
+  TextInput,
+  Text
 } from "react-native";
-import PhoneInput from "react-native-phone-number-input";
 
-
-const styles = require('./styles');
 const App : FC = ( { navigation } : any ) => {
-    
-    const [value, setValue] = useState("");
-    const [formattedValue, setFormattedValue] = useState("");
-    const [valid, setValid] = useState(false);
-    const [showMessage, setShowMessage] = useState(false);
-    const phoneInput = useRef<PhoneInput>(null);
+
+    const [ecName, changeECName] = useState("");
+
+
     return (
-      <>
-        <View style={styles.container}>
-          <SafeAreaView style={styles.wrapper}>
-            {showMessage && (
-              <View style={styles.message}>
-                <Text>Value : {value}</Text>
-                <Text>Formatted Value : {formattedValue}</Text>
-                <Text>Valid : {valid ? "true" : "false"}</Text>
-              </View>
-            )}
-            <PhoneInput
-              ref={phoneInput}
-              defaultValue={value}
-              defaultCode="GB"
-              layout="first"
-              onChangeText={(text) => {
-                setValue(text);
-              }}
-              onChangeFormattedText={(text) => {
-                setFormattedValue(text);
-              }}
-              withDarkTheme
-              withShadow
-              autoFocus
-            />
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => {
-                const checkValid = phoneInput.current?.isValidNumber(value);
-                setShowMessage(true);
-                setValid(checkValid ? checkValid : false);
-              }}
-            >
-              <Text>Check</Text>
-            </TouchableOpacity>
-          </SafeAreaView>
+        <View>
+          <Text>{"Emergency Contacts Email"}</Text>
+          <TextInput
+          keyboardType="email-address"
+          style={{backgroundColor: "green"}}>
+          </TextInput>
+          <Text>{"Emergency Contact First Name"}</Text>
+          <TextInput
+          style={{backgroundColor: "green"}}>
+          </TextInput>
         </View>
-      </>
     );
 };
 
