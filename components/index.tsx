03212ca7c49a577b501e6718fb524d83e9d72af8
Noted70 <bamlakkal2002@gmail.com>
2021-07-26 23:52:00 +0100

    deleted app

--- components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Input from './input'
-import Button from './button'
-import HeadingCurve from './headerCurve'
-import Header from './header'
-import AuthError from './authErrors'
-
-
-
-export {Input, Button, HeadingCurve, Header, AuthError}
--- ios/SwiftCompilation.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SwiftCompilation.swift
-//  SafeStep
-//
-//  Created by Ollie Pugh on 04/04/2021.
-//
-
-import Foundation
