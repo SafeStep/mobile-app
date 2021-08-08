12c36c3048f60af3892045efdfbb4f89861f54c7
Noted70 <bamlakkal2002@gmail.com>
2021-08-08 18:28:03 +0100

    Update mock-urls.tsx

--- mock-urls.tsx
+++ mock-urls.tsx
@@ -5,7 +5,7 @@ export const initMocks = () => {
     createServer({
         routes() {
           this.namespace = API_URL!;  // ! means defo not null
-          this.passthrough("https://cognito-idp.eu-west-1.amazonaws.com/**");
+          this.passthrough("https://cognito-idp.eu-west-1.amazonaws.com/");
           this.passthrough("http://10.0.2.2:8081/**");
           this.passthrough("https://api.mapbox.com/**");
           
