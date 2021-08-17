a032ae3a11a9d614243fbaf286a46834cc908c25
Ollie Pugh <oliver.pugh@mac.com>
2021-08-17 13:21:56 +0100

    Added on journey page

---
+++ screens/OnRouteScreen.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { OnRouteMap } from '../components'
+import { UserGeolocationService } from "../logic/UserGeolocationService";
+
+let styles = require('./styles');
+
+const App : FC = ( { route, navigation } : any ) => {
+
+    const endJourney = () => {
+        navigation.navigate('map')
+        try {
+            UserGeolocationService.instance.stopBackgroundWatch()
+        }
+        catch (err){
+            console.error(err)
+        }
+        UserGeolocationService.instance.startForegroundWatch()
+    }
+
+    return (
+        <SafeAreaView style={styles.mapContainer} edges={['right', "top", 'left']}>
+            <View style={styles.map}>
+                {<OnRouteMap path={route.params.path} /> }
+                
+                <TouchableOpacity style={styles.goButton} onPress={endJourney}>
+                    <Text style={styles.goButtonText}> End </Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default App;
