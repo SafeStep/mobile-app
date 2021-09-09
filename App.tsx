949b0e647db2304e8a3c50fe7dcc1f1ab855ed63
Noted70 <bamlakkal2002@gmail.com>
2021-09-09 22:27:42 +0100

    stopped miraje

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import Index from './navigation/index'
 export default function App() {
 
   if (process.env.NODE_ENV == "development" || process.env.NODE_ENV == "test") {  // create mirage endpoints
-    initMocks();  // create the mock endpoints
+    //initMocks();  // create the mock endpoints
   }
 
   const isLoadingComplete = useCachedResources();
